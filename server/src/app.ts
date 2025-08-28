03414934279071a2c0b1a54a7ad47de0538c80a1
Hanzala Khan <pythondeveloper360@gmail.com>
2025-08-28 18:55:32 +0500

    feat: restructure app setup to include user and course routers, and add cookie parser and authentication middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,15 +2,36 @@ import cors from "cors";
 import express from "express";
 import { AuthRouter } from "./routes/Auth.js";
 import Logger from "./services/Logger.js";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./models/db.js";
+import { UserRouter } from "./routes/User.js";
+import { CourseRouter } from "./routes/courses.js";
+import { AuthMiddleware } from "./middlewares/Auth.js";
+
 const app = express();
 
+app.use(cookieParser());
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend origin
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(Logger);
+app.use("/api/user", UserRouter);
 app.use("/api/auth", AuthRouter);
 
+// Protected Routes
+
+app.use(AuthMiddleware);
+
+app.use("/api/courses", CourseRouter);
+
+connectDB();
 app.listen(3000, () => {
   console.log("Listenig on port 3000");
 });
