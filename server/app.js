2147be48ac3588763b26c744c32bf78bc261602f
Hanzala Khan <pythondeveloper360@gmail.com>
2025-08-28 09:58:16 +0500

    chore: update server dependencies and set module type
    - Added cors, express, and json-web-token as dependencies.
    - Updated package.json to specify "type": "module".

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { AuthRouter } from './routes/Auth.js'
+import Logger from './services/Logger.js'
+import cors from 'cors'
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+app.use(Logger)
+app.use('/api/auth', AuthRouter)
+
+
+app.listen(3000, () => {
+    console.log("Listenig on port 3000")
+})
