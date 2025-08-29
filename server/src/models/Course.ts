824c9128d906a96df72900b45320cf08ee6a1cda
Hanzala Khan <pythondeveloper360@gmail.com>
2025-08-29 22:03:46 +0500

    feat: rename 'name' to 'title' in ICourse interface and CourseSchema

--- server/src/models/Course.ts
+++ server/src/models/Course.ts
@@ -1,14 +1,14 @@
 import mongoose, { type Document, Schema } from "mongoose";
 
 interface ICourse extends Document {
-  name: string;
+  title: string;
   code: string;
   resources: mongoose.Types.ObjectId[];
   lectures: mongoose.Types.ObjectId[];
 }
 
 const CourseSchema = new Schema<ICourse>({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   code: { type: String, required: true, unique: true },
   resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }],
   lectures: [{ type: Schema.Types.ObjectId, ref: "Lecture" }],
