55a31e340ad12664bced6c0e13ee10eaed468fc0
Hanzala Khan <pythondeveloper360@gmail.com>
2025-09-20 05:00:05 +0500

    feat: add createdBy field to Course model for user association

--- server/src/models/Course.ts
+++ server/src/models/Course.ts
@@ -3,6 +3,7 @@ import mongoose, { type Document, Schema } from "mongoose";
 interface ICourse extends Document {
   title: string;
   code: string;
+  createdBy: mongoose.Types.ObjectId;
   resources: mongoose.Types.ObjectId[];
   lectures: mongoose.Types.ObjectId[];
 }
@@ -10,6 +11,7 @@ interface ICourse extends Document {
 const CourseSchema = new Schema<ICourse>({
   title: { type: String, required: true },
   code: { type: String, required: true, unique: true },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
   resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }],
   lectures: [{ type: Schema.Types.ObjectId, ref: "Lecture" }],
 });
